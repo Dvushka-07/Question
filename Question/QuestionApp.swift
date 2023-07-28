f8cac00fb052c11d15c2284cb2c817ed3d0cd54e
Zoya <ezoya@yahoo.com>
2023-07-28 11:00:10 +0100

    Initial Commit

---
+++ Question/QuestionApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionApp.swift
+//  Question
+//
+//  Created by scholar on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuestionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
